aa2e5ec5c74c14b0407acab5a08ca668bb58e5eb
jaemin_ki <jaemin_ki@sk.com>
2025-01-24 10:28:07 +0900

    xml

--- webapp/index.js
+++ webapp/index.js
@@ -1,6 +1,17 @@
-sap.ui.define(["sap/m/Text"], (Text) => {
-  "use strict";
-  new Text({
-    text: "Hello, World!!",
-  }).placeAt("content");
+// sap.ui.define(["sap/m/Text"], (Text) => {
+//   "use strict";
+//   new Text({
+//     text: "Hello, World!!",
+//   }).placeAt("content");
+// });
+
+sap.ui.define(["sap/ui/core/mvc/XMLView"], (XMLView) => {
+  console.log("console", XMLView);
+  ("use strict");
+  XMLView.create({
+    viewName: "ui5.walkthrough11.view.App",
+    // viewName: "wrong.path.view.App", // 에러 발생시키는 코드
+  })
+    .then((oView) => oView.placeAt("content"))
+    .catch((e) => console.error(e, "에러 발생쓰"));
 });
