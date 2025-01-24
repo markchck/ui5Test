eb79d476ba0fae59931d17775b2d52b6d5603177
jaemin_ki <jaemin_ki@sk.com>
2025-01-24 14:56:28 +0900

    JSON Model

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,30 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
-  (Controller, MessageToast) => {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+  ],
+  (Controller, MessageToast, JSONModel) => {
     "use strict";
 
     return Controller.extend("ui5.walkthrough.controller.App", {
+      onInit() {
+        // set data model on view
+        const oData = {
+          recipient: {
+            name: "world",
+          },
+        };
+
+        const oModel = new JSONModel(oData);
+
+        this.getView().setModel(oModel);
+        // console.log("this?", this);
+        // console.log("getView?", this.getView());
+        // console.log("setModel", this.getView().setModel(oModel));
+      },
+
       onShowHello() {
-        // show a native JavaScript alert
         MessageToast.show("Hello, World!!");
       },
     });
