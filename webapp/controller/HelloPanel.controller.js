545abbf57507efcb0b98402860f330be9b0f3278
marck <markchck@gmail.com>
2025-01-30 22:01:18 +0900

    Fragement Callbacks 완료 Icons할 차례

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -22,6 +22,11 @@ sap.ui.define(
         });
         this.oDialog.open();
       },
+
+      onCloseDialog() {
+        this.byId("helloDialog").close();
+        // this.oDialog.close();
+      },
     });
   }
 );
