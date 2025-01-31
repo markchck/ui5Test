5d5518278b94d34e6948103d9fe54b41d4804c55
jaemin_ki <jaemin_ki@sk.com>
2025-01-31 13:34:29 +0900

    Expression Binding 할 차례

---
+++ webapp/controller/InvoiceList.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
+  function (Controller, JSONModel) {
+    "use strict";
+
+    return Controller.extend("ui5.walkthrough.controller.InvoiceList", {
+      onInit() {
+        const oViewModel = new JSONModel({
+          currency: "EUR",
+        });
+
+        this.getView().setModel(oViewModel, "view");
+      },
+    });
+  }
+);
