4fe38201b00619e76d426e665019411b0a509426
marck <markchck@gmail.com>
2025-02-02 14:59:51 +0900

    Filtering할 차례

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -5,6 +5,9 @@ sap.ui.define([], () => {
       const oResourceBundle = this.getOwnerComponent()
         .getModel("i18n")
         .getResourceBundle();
+      console.log("this가 뭐지?", this);
+      console.log("this.getOwnerComponent()", this.getOwnerComponent());
+      console.log("o");
       switch (sStatus) {
         case "A":
           return oResourceBundle.getText("invoiceStatusA");
