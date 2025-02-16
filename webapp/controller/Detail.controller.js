2bd20b61eda685e322a1521fc29b92bed9d737e5
marck <markchck@gmail.com>
2025-02-16 21:21:50 +0900

    Routing back and history완료

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,23 +1,39 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
-  "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"],
+  (Controller, History) => {
+    "use strict";
 
-  return Controller.extend("ui5.walkthrough.controller.Detail", {
-    onInit() {
-      const oRouter = this.getOwnerComponent().getRouter();
-      oRouter
-        .getRoute("detail")
-        .attachPatternMatched(this.onObjectMatched, this);
-    },
+    return Controller.extend("ui5.walkthrough.controller.Detail", {
+      onInit() {
+        const oRouter = this.getOwnerComponent().getRouter();
+        oRouter
+          .getRoute("detail")
+          .attachPatternMatched(this.onObjectMatched, this);
+      },
 
-    onObjectMatched(oEvent) {
-      this.getView().bindElement({
-        path:
-          "/" +
-          window.decodeURIComponent(
-            oEvent.getParameter("arguments").invoicePath
-          ),
-        model: "invoice",
-      });
-    },
-  });
-});
+      onObjectMatched(oEvent) {
+        this.getView().bindElement({
+          path:
+            "/" +
+            window.decodeURIComponent(
+              oEvent.getParameter("arguments").invoicePath
+            ),
+          model: "invoice",
+        });
+      },
+      onNavBack() {
+        const oHistory = History.getInstance();
+        console.log("oHistory", oHistory);
+        const sPrevioushHash = oHistory.getPreviousHash();
+        console.log("sPrevioushHash", sPrevioushHash);
+
+        if (sPrevioushHash !== undefined) {
+          window.history.go(-1);
+        } else {
+          const oRouter = this.getOwnerComponent().getRouter();
+          oRouter.navTo("overview", {});
+        }
+      },
+    });
+  }
+);
